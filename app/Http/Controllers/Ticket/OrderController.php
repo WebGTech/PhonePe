095b9a38cca77316a8ed807213e808d0ff6cd8e5
Govind yadav <43997191+GtechGovind@users.noreply.github.com>
2022-02-10 00:40:47 +0530

    Update OrderController.php

--- app/Http/Controllers/Ticket/OrderController.php
+++ app/Http/Controllers/Ticket/OrderController.php
@@ -31,6 +31,8 @@ class OrderController extends Controller
 
         $saleOrderNumber = $this -> genSaleOrderNumber($request->input('pass_id'));
 
+        dd($saleOrderNumber);
+
         DB::table('sale_order')->insert([
             'sale_or_no' => $saleOrderNumber,
             'txn_date' => now(),
